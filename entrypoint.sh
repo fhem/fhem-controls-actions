e8601afba562d2344bce145f96e560e4d261132e
sidey79 <7968127+sidey79@users.noreply.github.com>
2019-11-22 22:33:54 +0100

    entrypoint.sh  syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  
-rm -f $1 
+rm -f "$1" 
 while IFS= read -r -d '' FILE
 do
-   TIME=$(git log --pretty=format:%cd -n 1 --date=format:%Y-%m-%d\_%H:%M:%S -- "$FILE")
+   TIME=$(git log --pretty=format:%cd -n 1 --date=format':%Y-%m-%d\_%H:%M:%S' -- "$FILE")
    FILESIZE=$(stat -c%s "$FILE")
    FILE=$(echo "$FILE"  | cut -c 3-)
-   printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> $1
+   printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> "$1"
 done <   <(find ./FHEM -maxdepth 2 \( -name "*.pm" \) -print0 | sort -z -g)
